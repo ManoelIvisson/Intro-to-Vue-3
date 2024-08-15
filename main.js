147196936dc2ed66204e18cea5f9b2c07a7068d5
Manoel Ívisson <manoelivisson4@hotmail.com>
2024-08-15 10:19:32 -0300

    feature: Conceitos básicos do Vue.js aplicados

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2245, color: 'Green'},
+                {id: 2265, color: 'Blue'}
+            ]
+        }
+    }
+})
+
 const product = 'Socks'
